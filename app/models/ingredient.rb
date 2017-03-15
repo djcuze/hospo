d9b5cb79e72e37eab4ef8493b7d3e8d54a08dd06
djcuze <natejacobymusic@gmail.com>
2017-03-15 16:05:25 +1000

    Ingredient Model checks for Uniqueness

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,6 @@ class Ingredient < ApplicationRecord
   has_and_belongs_to_many :recipes
 
   validates :name, presence: true,
-      length: { minimum: 3 }
+      length: { minimum: 3 }, uniqueness: true
 
 end
